80384a7eafced183397ffcc5abe2a6f9fef01668
lzy <951689833@qq.com>
2023-05-23 15:58:08 +0800

    xxx

--- rpc/transform/model/shorturl.sql
+++ rpc/transform/model/shorturl.sql
@@ -4,3 +4,12 @@ CREATE TABLE `shorturl`
     `url` varchar(255) NOT NULL COMMENT 'original url',
     PRIMARY KEY(`shorten`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `user`
+(
+    `id` INT NOT NULL  COMMENT '自增ID',
+    `name` varchar(255) NOT NULL COMMENT '姓名',
+    `gender` SMALLINT NOT NULL COMMENT '性别',
+    `phone` VARCHAR(11) COMMENT '电话',
+    PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
